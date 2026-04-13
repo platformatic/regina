305eb9a03e7215004bc8e79512112fa486d9b111
Paolo Insogna <paolo@cowtech.it>
2026-04-13 09:19:03 +0200

    chore: Added exports. (#2)
    * chore: Added exports.
    
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>
    
    * fix: Added getters.
    
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>
    
    ---------
    
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- packages/regina/src/index.ts
+++ packages/regina/src/index.ts
@@ -1,8 +1,8 @@
 import { create as createService, platformaticService } from '@platformatic/service'
+import type { FastifyInstance } from 'fastify'
 import fp from 'fastify-plugin'
 import { plugin } from './plugin.ts'
 import { schema } from './schema.ts'
-import type { FastifyInstance } from 'fastify'
 
 export async function regina (app: FastifyInstance, capability: any) {
   await platformaticService(app, capability)
@@ -13,5 +13,9 @@ export async function create (configOrRoot: any, sourceOrConfig?: any, context?:
   return createService(configOrRoot, sourceOrConfig, { schema, applicationFactory: fp(regina), ...context })
 }
 
-export { Generator } from './generator.ts'
-export { packageJson, schema, schemaComponents, version } from './schema.ts'
+export * from './agent-discovery.ts'
+export * from './generator.ts'
+export * from './instance-manager.ts'
+export * from './plugin.ts'
+export * from './schema.ts'
+export * from './state-backup.ts'
